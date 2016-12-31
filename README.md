77ed2a7369cdee5cdbcf7eb3ec5396849c93445f
hiroyuki <hiroyuki@mbp1.local>
2017-01-01 00:12:44 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Flicker Light
+
+## Usage
+Load flickerlight.css and flickerlight.js.  
+Add custom data attributes to target element.
+
+- must (no need to assign value)  
+data-flickerlight-target
+- optional  
+data-flickerlight-amount
+
+e.g.  
+`<div data-flickerlight-target data-flickerlight-amount='100'></div>`
+
+## License
+
+MIT © greenishsea
+
---
+++ js/flickerlight.js
@@ -0,0 +1,54 @@
+/**
+ * Flicker Light
+ *
+ * Copyright 2017-01-01 greenishsea
+ * Dual licensed under MIT and GPL.
+ *
+ * @description generate specific amounts of flicker light in the target element
+ * @version 1.0
+ * @param {Object} target: target element
+ *                 amount: of flicker light
+ * @return None
+ *
+ */
+window.addEventListener("DOMContentLoaded", function(){
+
+  var defaults = {
+    amount: 90
+  };
+
+  var nodeList = document.querySelectorAll('[data-flickerlight-target]');
+  if (nodeList[0] === undefined) {
+    return false;
+  }
+  var el = nodeList[0]; // currently firstTarget only (TODO: forEach)
+
+  var amountString = el.dataset.flickerlightAmount;
+  var amount = amountString ? parseInt(amountString, 10) : defaults.amount;
+
+  var flickerRenderer = {
+    render: function(){
+      for (var i = 0; i <= amount; i++) {
+        var flicker = this.getNewFlicker();
+        flicker.style.top = this.rand() * 100 + "%";
+        flicker.style.left = this.rand() * 100 + "%";
+        flicker.style.webkitAnimationDelay = this.rand() + "s";
+        flicker.style.mozAnimationDelay = this.rand() + "s";
+        el.appendChild(flicker);
+      };
+    },
+    
+    rand: function(){
+      return Math.random();
+    },
+
+    getNewFlicker: function(){
+      var d = document.createElement('div');
+      d.innerHTML = '<figure class="flickerlight"></figure>';
+      return d.firstChild;
+    },
+  };
+  
+  flickerRenderer.render();
+
+}, false);
